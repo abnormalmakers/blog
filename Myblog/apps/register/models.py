37465673b0cfd8eeaec973d2399257fa56785c47
qmy <275394982@qq.com>
2018-12-22 03:02:00 +0800

    models映射生成users表

--- Myblog/apps/register/models.py
+++ Myblog/apps/register/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    phone=models.CharField(max_length=11)
+    password=models.CharField(max_length=150)
+    is_active=models.BooleanField()
+
+    def __str__(self):
+        return self.phone
+
+    class Meta:
+        db_table='users'
+        verbose_name='用户'
+        verbose_name_plural=verbose_name
