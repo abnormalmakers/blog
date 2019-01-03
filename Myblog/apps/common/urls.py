80620910e066bf5f2a7a1a9ea88edbdb38740386
qmy <275394982@qq.com>
2019-01-04 07:49:55 +0800

    增加服务器错误页面，路由

--- Myblog/apps/common/urls.py
+++ Myblog/apps/common/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import headers_view
+from .views import headers_view,Servererror_view
 
 urlpatterns=[
-    url(r'^headers/',headers_view)
+    url(r'^headers/',headers_view),
+    url(r'^servererror/',Servererror_view.as_view(),name='servererror')
 ]
--- Myblog/apps/common/views.py
+++ Myblog/apps/common/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.base import View
 
 # Create your views here.
 def headers_view(request):
     return render(request,'header.html',locals())
+
+class Servererror_view(View):
+    def get(self,request):
+        return render(request,'server_error.html',locals())
