4cf5653bcb628f2d0a5abd01fa65d0d39421ed0b
qmy <275394982@qq.com>
2019-01-04 15:00:19 +0800

    分类 页面跳转 js

--- Myblog/static/js/personal/personal.js
+++ Myblog/static/js/personal/personal.js
@@ -4,8 +4,18 @@ var personal={
 
 
 $(function(){
+    //页面跳转
     $('#sure_turnpage').on('click',function(){
         target_page=$('#skip_page').val();
-        window.location.href='/personal/?page='+target_page
-    })
+        param_arr=location.search.split('&')
+        if(param_arr.length>1){
+            window.location.href='/personal/?page='+target_page+'&'+param_arr[1]
+        }else{
+            window.location.href='/personal/?page='+target_page
+        }
+
+
+
+
+    });
 });
